23f702aef6c3108d78e47bf7656db18e4a11511d
Nobeen <n.islam.1@outlook.com>
2022-05-26 01:47:39 +0100

    basic buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 
 function App(): JSX.Element {
   const[string,setString] = useState<string>("")
+  const[secret,setSecret] = useState<string>("")
+
   const baseUrl =
     process.env.NODE_ENV === "production"
       ? "https://auth-test-back.herokuapp.com/"
@@ -14,10 +16,19 @@ function App(): JSX.Element {
     setString(stringFromServer)
   }
 
+  async function handleClickSecret(){
+    const response = await axios.get(baseUrl + "secret")
+    const secretFromServer = response.data
+    console.log("hello",response)
+    setSecret(secretFromServer)
+  }
+
   return (
   <>
     <button onClick={handleClick}>Click Me</button>
-    <div>{string}</div>
+    <p>{string}</p>
+    <button onClick={handleClickSecret}>Secret Button</button>
+    <p>{secret}</p>
   </>
   );
 }
