657e558d23f4d9c7121272db7a30a020e98ebd49
Nobeen <n.islam.1@outlook.com>
2022-05-26 01:39:27 +0100

    Button gets word

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import { greet } from "./utils/greet";
+import axios from "axios";
+import { useState } from "react";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  const[string,setString] = useState<string>("")
+  const baseUrl =
+    process.env.NODE_ENV === "production"
+      ? "https://auth-test-back.herokuapp.com/"
+      : "http://localhost:4000/";
+
+  async function handleClick(){
+    const response = await axios.get(baseUrl)
+    const stringFromServer = response.data
+    setString(stringFromServer)
+  }
+
+  return (
+  <>
+    <button onClick={handleClick}>Click Me</button>
+    <div>{string}</div>
+  </>
+  );
 }
 
 export default App;
